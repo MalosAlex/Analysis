bddee6ea111f52c5fd5d8e9983ec6deb8879caac
Ghestr <malosalexandru2004@gmail.com>
2023-10-28 14:56:11 +0300

    Fixed a big problem
    Fixed the problem where if p and q are different one may be left behind and jump over some numbers

--- main.py
+++ main.py
@@ -19,35 +19,28 @@ for j in range(0, 4):
 
 data = [
     ["n", "p", "q", "result", "e^result = ln x"],
-    [n_input, 0, 0, results[0], math.exp(results[0])],
-    [n_input * 2, 0, 0, results[1], math.exp(results[1])],
-    [n_input * 4, 0, 0, results[2], math.exp(results[2])],
-    [n_input * 8, 0, 0, results[3], math.exp(results[3])],
+    [n_input, 1, 1, results[0], math.exp(results[0])],
+    [n_input * 2, 1, 1, results[1], math.exp(results[1])],
+    [n_input * 4, 1, 1, results[2], math.exp(results[2])],
+    [n_input * 8, 1, 1, results[3], math.exp(results[3])],
 ]
 
 for j in range(0, 4):
     result = float(0)
     n = n_input * (2 ** j)
-    i = 1
-    while i <= n:
-        p1 = q1 = 0
-        i1 = i  # We clone i so that I can start the additions and subtractions from i but also be able
-        # to check whether if it has gotten bigger than n in the while
-        while p1 < p and i + p1 <= n:
-            d = i % 2
-            result = result + 1/(i+2*p1+(1-d))
-            p1 = p1 + 1
-            # With this d I make sure that n+2*p is always odd so that it's positive
-        i1 = i1 + p1
-        while q1 < q and i1 + q1 <= n:  # We check that the number of elements in the sum
-            # isn't bigger than n. We already have i+p1=i1 elements in the sum
-            d = i % 2
-            result = result - 1/(i+2*q1+d)
-            q1 = q1 + 1
-            #  With this d I make sure that n+2*q is always even so that it's negative
-            if i1 + q1 + d > n:
-                break
-        i = i1 + q1
+    t = n // (q+p)  # How many times each p and q have to be executed
+    rest = n % (q+p)  # How many operations have remained
+    if rest > p:
+        for i in range(t*p+1):
+            result = result + 1/(2 * i + 1)
+        rest = rest - p
+        for j in range(1, t*q + rest + 1):
+            result = result - 1/(2 * j)
+    else:
+        for i in range(t*p+rest+1):
+            result = result + 1 / (2 * i + 1)
+        for j in range(1, t * q + 1):
+            result = result - 1 / (2 * j)
     results.append(result)
     data.append([n, p, q, result, math.exp(result)])
 
